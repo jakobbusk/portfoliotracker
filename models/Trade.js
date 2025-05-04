8f4cf855bddd9b87fc35f4a3265d7dca0389268e
Jakob Busk <jakob@sbusk.dk>
2025-05-04 10:13:49 +0200

    Fix trade positions

--- models/Trade.js
+++ models/Trade.js
@@ -131,7 +131,7 @@ export default class Trade {
 
 
             // Opdater positionen
-            // await position.update(this.tradeType, this.quantity, tradeValue);
+            await position.update(this.tradeType, this.quantity, tradeValue);
             // await portfolio.update();
 
             return true;
