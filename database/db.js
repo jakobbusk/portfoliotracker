31fbc6d1620a5baa036a045b2746d2962061a6b1
Jakob Busk <jakob@sbusk.dk>
2025-05-10 23:27:07 +0200

    fjern mysql

--- database/db.js
+++ database/db.js
@@ -1,11 +1,9 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
 import sql from 'mssql';
-
 const config = {
     server: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER  || 'root',
+    user: process.env.DB_USER  || 'sa',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_DATABASE || 'portfoliotracker',
     options: {
@@ -13,10 +11,7 @@ const config = {
         trustServerCertificate: true,
     }
 };
-
-
 const pool = new sql.ConnectionPool(config);
-
 export default pool
 export { sql }
 
